0f8951c9d11ce3d7b9d50c388cc4f606ae5be21d
Haval Hezha Zrar <76939017+HavalHezha@users.noreply.github.com>
2024-03-09 12:16:51 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ sudo apt install python
+ sudo apt install python3
+
+ sudo apt install git
+
+ git clone https://github.com/HavalHezha/Python-calculator.git
+
+ cd Python-calculator
+
+ clear
+
+ python3 calculator.py
